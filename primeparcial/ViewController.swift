ef1c89016054b42e2ece0fe86c99e934611dd237
Alumno <cesarrgomez@hotmail.com>
2021-09-24 09:58:17 -0700

    Initial Commit

---
+++ primeparcial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  primeparcial
+//
+//  Created by Alumno on 9/24/21.
+//  Copyright © 2021 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
